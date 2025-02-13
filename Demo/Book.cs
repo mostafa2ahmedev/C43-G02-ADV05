3433c69415642f7ab9a35964930b9e3f7412daec
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-02-13 05:07:52 +0200

    Part 01: Q1

---
+++ Demo/Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    #region Part 01: 
+    internal class Book
+    {
+
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string[] Authors { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public decimal Price { get; set; }
+
+
+        public Book(string _ISBN, string _title, string[] _authors, DateTime _publicationDate, decimal _price)
+        {
+            this.Authors = _authors;
+            this.Title = _title;
+            this.PublicationDate = _publicationDate;
+            this.Price = _price;
+            this.ISBN = _ISBN;
+        }
+
+
+        public override string ToString()
+        {
+            string authors = "";
+
+            foreach (var author in Authors)
+            {
+                authors += author + ",";
+            }
+
+            return $"ISBN : {ISBN}, Title : {Title}, Authors : {Authors}, PublicationDate : {PublicationDate}, Price : {Price}";
+        }
+    } 
+    #endregion
+}
---
+++ Demo/BookFunctions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    #region Part 01;
+    internal class BookFunctions
+    {
+
+        public static string GetTitle(Book B)
+        {
+            return B?.Title ?? "No Name";
+
+        }
+        public static string GetAuthors(Book B)
+        {
+
+            string authors = "";
+
+            foreach (var author in B.Authors)
+            {
+                authors += author + ",";
+            }
+
+
+            return authors ?? "No Authors";
+
+        }
+        public static string GetPrice(Book B)
+        {
+
+            return B?.Price.ToString() ?? "No Price";
+
+        }
+    } 
+    #endregion
+}
